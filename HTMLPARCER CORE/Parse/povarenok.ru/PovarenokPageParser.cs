d8f2c43f75f1e37c016f00a8114a51ddfa204f8b
Андрей Романюк <asromanyuk@edu.hse.ru>
2020-01-08 00:39:14 +0300

    Почти доделан парсер для поварёнка.

--- HTMLPARCER CORE/Parse/povarenok.ru/PovarenokPageParser.cs
+++ HTMLPARCER CORE/Parse/povarenok.ru/PovarenokPageParser.cs
@@ -19,33 +19,42 @@ namespace HTMLPARCER_CORE
 
         public RecipeFull[] Parse(IHtmlDocument document)
         {
-            Console.WriteLine("[DEBUG]: code 1");
+
             var titleArray = document.QuerySelectorAll("h1").Where(item =>
                 item.ParentElement.ParentElement.ClassList.Contains(
                     "item-about")).ToArray();
-
-            Console.WriteLine($"[DEBUG]: {titleArray.Length}");
 
             var titlePictureArray = document.QuerySelectorAll("img").Where(item =>
                 item.ParentElement.ClassList.Contains(
                     "m-img")).ToArray();
 
-            Console.WriteLine($"[DEBUG]: {titlePictureArray.Length}");
-
             var introductionContentArray = document.QuerySelectorAll("p").Where(item =>
                 item.ParentElement.ClassList.Contains(
                     "article-text")).ToArray();
-            Console.WriteLine($"[DEBUG]: {introductionContentArray.Length}");
-
 
             var ingridientsArray = document.QuerySelectorAll("div").Where(item =>
                 item.ClassList != null &&
                 item.ClassList.Contains("ingredients-bl")).ToArray();
-
             
+
+            var stepsOfRecipeArray = document.QuerySelectorAll("div")
+                .Where(item => item.ClassList != null && item.ClassList.Contains("cooking-bl")).ToArray();
+
+
+            var endContentArray = document.QuerySelectorAll("div").Where(item =>
+                    item.ParentElement.ParentElement.ClassName != null &&
+                    item.ParentElement.ParentElement.ClassName.Contains("item-bl item-about"))
+                .ToArray();
+
+
+            title = titleArray[0].TextContent;
+            titlePicture = titlePictureArray[0].Attributes[1].Value;
+            introductionContent = introductionContentArray[0].TextContent;
+
             var pIngridientsArray = ingridientsArray[0].QuerySelectorAll("p").ToArray();
             var ulIngridientsArray = ingridientsArray[0].QuerySelectorAll("ul").ToArray();
             List<Ingredient> ingridientsList = new List<Ingredient>();
+
             for (int i = 0; i < ulIngridientsArray.Length; i++)
             {
                 var liIngridientsArray = ulIngridientsArray[i].QuerySelectorAll("li").ToArray();
@@ -54,11 +63,10 @@ namespace HTMLPARCER_CORE
                     string text = li.TextContent.Replace("\n", "").Replace("  ", "");
                     string name;
                     string unit;
-                    
-                    if (text.Contains("-"))
+                    if (text.Contains("—"))
                     {
-                        int index = text.IndexOf('-');
-                        name = text.Substring(0, index - 1);
+                        int index = text.IndexOf('—');
+                        name = text.Substring(0, index);
                         unit = text.Substring(index + 1);
                     }
                     else
@@ -66,42 +74,22 @@ namespace HTMLPARCER_CORE
                         name = text;
                         unit = null;
                     }
-                        
-                    ingridientsList.Add(new Ingredient(pIngridientsArray[i].TextContent, name, unit ));
-                    Console.WriteLine($"Title: {pIngridientsArray[i].TextContent}, Product: {name}, unit: {unit}");
+
+                    ingridientsList.Add(new Ingredient(pIngridientsArray[i].TextContent, name, unit));
                 }
 
             }
-            
-            Console.WriteLine($"");
 
+            ingredients = ingridientsList.ToArray();
 
-            var stepsOfRecipeArray = document.QuerySelectorAll("div")
-                .Where(item => item.ClassList != null && item.ClassList.Contains("cooking-bl")).ToArray();
-
-            Console.WriteLine($"[DEBUG]: {stepsOfRecipeArray.Length}");
-
-
-            var endContentArray = document.QuerySelectorAll("div").Where(item =>
-                    item.ParentElement.ParentElement.ClassName != null &&
-                    item.ParentElement.ParentElement.ClassName.Contains("item-bl item-about"))
-                .ToArray();
-
-            Console.WriteLine($"[DEBUG]: {endContentArray.Length}");
-
-            title = titleArray[0].TextContent;
-            titlePicture = titlePictureArray[0].Attributes[1].Value;
-            introductionContent = introductionContentArray[0].TextContent;
-            Console.WriteLine(
-                $"[DEBUG]: title: {title}, titlePicture: {titlePicture}, introductionContent: {introductionContent}");
 
 
             stepsOfRecipe = new StepRecipe[stepsOfRecipeArray.Length];
             for (int i = 0; i < stepsOfRecipe.Length; i++)
             {
-                string description = stepsOfRecipeArray[i].LastElementChild.TextContent;
-                string urlPicture = stepsOfRecipeArray[i].FirstElementChild.Attributes[2].Value;
-                Console.WriteLine($"[DEBUG]: id:{i} name:{description}, unit{stepsOfRecipeArray}");
+                string description = stepsOfRecipeArray[i].LastElementChild.TextContent.Replace("  ", "").Replace("\n", "");
+                string urlPicture = stepsOfRecipeArray[i].QuerySelector("img").Attributes[2].Value;
+                Console.WriteLine($"[DEBUG]: id:{i} name:{description}\npictrure: {urlPicture}");
                 stepsOfRecipe[i] = new StepRecipe(description, urlPicture);
             }
 
