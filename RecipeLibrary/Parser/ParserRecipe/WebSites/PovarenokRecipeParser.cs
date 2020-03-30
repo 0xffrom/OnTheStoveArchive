c53fa9da968c6560f21e8ded264614a74628a084
Андрей Романюк <asromanyuk@edu.hse.ru>
2020-03-30 17:03:31 +0300

    Поправлен парсинг сайта Povarenok.Ru

--- RecipeLibrary/Parser/ParserRecipe/WebSites/PovarenokRecipeParser.cs
+++ RecipeLibrary/Parser/ParserRecipe/WebSites/PovarenokRecipeParser.cs
@@ -17,24 +17,29 @@ namespace RecipeLibrary.Parser.ParserRecipe.WebSites
         private StepRecipeBox[] StepRecipesBoxes { get; set; }
         private AdditionalBox Additional { get; set; }
 
+        private const string WhiteSpaceBug = "  ";
+
         public RecipeFull Parse(IHtmlDocument document)
         {
-            var recipeBody = document.QuerySelectorAll("article")
-                .Where(element => element.ClassName != null && element.ClassName == "item-bl item-about")
-                .ToArray()[0];
+            var recipeBody = document
+                .QuerySelectorAll("article")
+                .FirstOrDefault(element => element.ClassName != null && element.ClassName == "item-bl item-about");
+
+            // recipeBody =>  главный фрейм с рецептом, если он существует - работаем с ним, иначе - рецепта не сущесвует.
+            if (recipeBody == null)
+                return new RecipeFull(string.Empty, Title, TitlePicture, Description, IngredientsBoxes,
+                    StepRecipesBoxes,
+                    Additional);
+
 
             #region Title
 
-            string title = recipeBody.QuerySelector("h1").TextContent;
-            string urlTitlePicture = recipeBody.QuerySelectorAll("div")
+            Title = recipeBody.QuerySelector("h1").TextContent;
+
+            TitlePicture = new Picture(recipeBody.QuerySelectorAll("div")
                 .Where(element => element.ClassName != null && element.ClassName == "m-img")
-                .Select(element => element.FirstElementChild.Attributes[1].Value)
-                .ToArray()[0];
-
-            Picture titlePicture = new Picture(urlTitlePicture);
-
-            Title = title;
-            TitlePicture = titlePicture;
+                .Select(element => element.FirstElementChild?.Attributes[1]?.Value)
+                .FirstOrDefault());
 
             #endregion
 
@@ -50,13 +55,13 @@ namespace RecipeLibrary.Parser.ParserRecipe.WebSites
 
             #region IngredientBox
 
-            var ingredientBody = recipeBody.QuerySelectorAll("div")
-                .Where(element => element.ClassName != null && element.ClassName == "ingredients-bl")
-                .ToArray()[0];
+            var ingredientBody = recipeBody
+                .QuerySelectorAll("div")
+                .FirstOrDefault(element => element.ClassName != null && element.ClassName == "ingredients-bl");
 
-            int countIngredientTitles = ingredientBody.QuerySelectorAll("ul").ToArray().Length;
+            int countIngredientTitles = ingredientBody?.QuerySelectorAll("ul").Length ?? 0;
 
-            IngredientBox[] ingredientBoxes = new IngredientBox[countIngredientTitles];
+            var ingredientBoxes = new IngredientBox[countIngredientTitles];
 
             for (int i = 0; i < countIngredientTitles; i++)
             {
@@ -92,20 +97,6 @@ namespace RecipeLibrary.Parser.ParserRecipe.WebSites
 
                 for (int j = 0; j < ingredientsArray.Length; j++)
                 {
-                    //string jumpSpaceBug = ("\n");
-                    //string whiteSpaceBug = ("  ");
-
-                    //string name = ingredientsArray[j].FirstElementChild.FirstElementChild.TextContent
-                    //    .Replace(jumpSpaceBug, String.Empty)
-                    //    .Replace(whiteSpaceBug, String.Empty);
-
-                    //string unit = ingredientsArray[j].TextContent
-                    //    .Replace(name, String.Empty)
-                    //    .Replace(whiteSpaceBug, String.Empty)
-                    //    .Replace(jumpSpaceBug, String.Empty)
-                    //    .Replace("—", " ")
-                    //   .Replace("/", String.Empty);
-
                     string name = ingredientsArray[j].QuerySelectorAll("span").Where(item =>
                             item.Attributes[0] != null && item.Attributes[0].Value == ("name"))
                         .Select(item => item.TextContent).First();
@@ -115,11 +106,11 @@ namespace RecipeLibrary.Parser.ParserRecipe.WebSites
                         .Select(item => item.TextContent).First();
 
                     name += ingredientsArray[j].TextContent.Replace(name, string.Empty)
-                        .Replace(unit, String.Empty)
-                        .Replace("\n", String.Empty)
-                        .Replace("  ", String.Empty)
+                        .Replace(unit, string.Empty)
+                        .Replace("\n", string.Empty)
+                        .Replace(WhiteSpaceBug, string.Empty)
                         .Replace("—", String.Empty);
-                    
+
                     Ingredient ingredient = new Ingredient(name, unit);
                     ingredients[j] = ingredient;
                 }
@@ -140,17 +131,17 @@ namespace RecipeLibrary.Parser.ParserRecipe.WebSites
                 .ToArray();
 
             int countRecipes = recipesArray.Length;
+
             StepRecipeBox[] stepRecipeBoxes = new StepRecipeBox[countRecipes];
 
             for (int i = 0; i < countRecipes; i++)
             {
-                string pictureUrl = recipesArray[i].FirstElementChild.FirstElementChild.Attributes[2].Value;
-                string description = recipesArray[i].LastElementChild.FirstElementChild.TextContent;
+                string pictureUrl = recipesArray[i]?.FirstElementChild?.FirstElementChild?.Attributes[2]?.Value;
+                string description = recipesArray[i]?.LastElementChild?.FirstElementChild?.TextContent;
 
-                Picture picture = new Picture(pictureUrl);
-                PictureBox pictureBox = new PictureBox(new Picture[1] {picture});
+                var pictureBox = new PictureBox(new Picture[1] {new Picture(pictureUrl)});
 
-                StepRecipeBox stepRecipeBox = new StepRecipeBox(description, pictureBox);
+                var stepRecipeBox = new StepRecipeBox(description, pictureBox);
 
                 stepRecipeBoxes[i] = stepRecipeBox;
             }
@@ -166,52 +157,43 @@ namespace RecipeLibrary.Parser.ParserRecipe.WebSites
                     element.NextElementSibling?.ClassName != null &&
                     element.NextElementSibling.ClassName == ("article-tags"));
 
-            if (additionalBody != null)
+            if (additionalBody == null)
+                return new RecipeFull(string.Empty, Title, TitlePicture, Description, IngredientsBoxes,
+                    StepRecipesBoxes,
+                    Additional);
+
+
+            var imagesArray = additionalBody.QuerySelectorAll("a")
+                .ToArray();
+
+            Picture[] pictures = new Picture[imagesArray.Length];
+
+            for (int i = 0; i < imagesArray.Length; i++)
             {
-                var imagesArray = additionalBody.QuerySelectorAll("a")
-                    .ToArray();
+                Picture picture = new Picture("http://www.povarenok.ru" + imagesArray[i].Attributes[0].Value);
 
-                Picture[] pictures = new Picture[imagesArray.Length];
-
-                for (int i = 0; i < imagesArray.Length; i++)
-                {
-                    Picture picture = new Picture("http://www.povarenok.ru" + imagesArray[i].Attributes[0].Value);
-
-                    pictures[i] = picture;
-                }
-
-                PictureBox picturesBox = new PictureBox(pictures);
-
-                string whiteSpaceBug = ("  ");
-
-
-                string textAdditional = additionalBody.TextContent
-                    .Replace(whiteSpaceBug, String.Empty);
-
-                var videoArray = recipeBody.QuerySelectorAll("div")
-                    .Where(element => element.ClassName != null && element.ClassName == ("video-bl"))
-                    .Select(element => element.FirstElementChild.FirstElementChild.Attributes[2].Value)
-                    .ToArray();
-
-                string videoUrl = null;
-                if (videoArray.Length > 0)
-                    videoUrl = videoArray[0];
-
-                Video video = new Video(videoUrl);
-                AdditionalBox additionalBox = new AdditionalBox(textAdditional, picturesBox, video);
-
-                Additional = additionalBox;
+                pictures[i] = picture;
             }
+
+            PictureBox picturesBox = new PictureBox(pictures);
+
+            string textAdditional = additionalBody.TextContent
+                .Replace(WhiteSpaceBug, String.Empty);
+
+            Video video = new Video(recipeBody.QuerySelectorAll("div")
+                .Where(element => element.ClassName != null && element.ClassName == ("video-bl"))
+                .Select(element => element.FirstElementChild.FirstElementChild.Attributes[2].Value)
+                .FirstOrDefault());
+
+            Additional = new AdditionalBox(textAdditional, picturesBox, video);
 
             #endregion
 
 
-            RecipeFull recipeFull = new RecipeFull(string.Empty, Title, TitlePicture, Description, IngredientsBoxes,
+            return new RecipeFull(string.Empty, Title, TitlePicture, Description, IngredientsBoxes,
                 StepRecipesBoxes,
                 Additional);
-
-            return recipeFull;
+            ;
         }
-        
     }
 }
