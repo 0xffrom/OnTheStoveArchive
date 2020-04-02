c628191ec30dd83a92f860729c6527da97cc3c62
Андрей Романюк <asromanyuk@edu.hse.ru>
2020-04-02 21:03:31 +0300

    Добавлен progressbar.

--- XamarinApp/MainActivity.cs
+++ XamarinApp/MainActivity.cs
@@ -21,10 +21,14 @@ namespace XamarinApp
     [Activity(Label = "На плите!", Theme = "@style/AppTheme.NoActionBar", Icon = "@drawable/icon", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
     {
-        private RecyclerView _listView;
+        private RecyclerView recyclerView;
         private RecipeShort[] _recipes;
         private DrawerLayout _drawer;
+        private ProgressBar progressBar;
 
+
+        private int page = 1;
+        private const string message = "Идёт загрузка рецептов, подождите, пожалуйста.";
         public static string LastUrl { get; private set; }
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -40,18 +44,18 @@ namespace XamarinApp
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
 
-
             SetContentView(Resource.Layout.activity_search);
+            progressBar = FindViewById<ProgressBar>(Resource.Id.loadingProgressBar);
 
-            _listView = FindViewById<RecyclerView>(Resource.Id.listRecipeShorts);
-            _listView.HasFixedSize = true;
+            recyclerView = FindViewById<RecyclerView>(Resource.Id.listRecipeShorts);
+          
+            recyclerView.HasFixedSize = true;
 
             UpdateListView();
 
+            SetPlateRecipe();
 
-             SetPlateRecipe();
- 
-             SetSpinner();
+            SetSpinner();
 
             _drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
             var toggle = new ActionBarDrawerToggle(this, _drawer, Resource.String.navigation_drawer_open,
@@ -63,36 +67,34 @@ namespace XamarinApp
             navigationView.SetNavigationItemSelectedListener(this);
 
             var buttonMenu = FindViewById<Button>(Resource.Id.menu_button);
+            buttonMenu.Click += SetButtonClick;
+        }
 
+        private void SetButtonClick(object sender, EventArgs args)
+        {
+            _drawer.OpenDrawer(GravityCompat.Start);
 
-            buttonMenu.Click += delegate(object sender, EventArgs args)
+            var menu_buttonM = FindViewById<Button>(Resource.Id.menu_buttonM);
+            menu_buttonM.Animation = new RotateAnimation(0, 180);
+            menu_buttonM.Click += delegate (object sender, EventArgs args)
             {
-                _drawer.OpenDrawer(GravityCompat.Start);
-
-                var menu_buttonM = FindViewById<Button>(Resource.Id.menu_buttonM);
-                menu_buttonM.Animation = new RotateAnimation(0, 180);
-                menu_buttonM.Click += delegate(object sender, EventArgs args)
-                {
-                    if (_drawer.IsDrawerOpen(GravityCompat.Start))
-                        OnBackPressed();
-                };
+                if (_drawer.IsDrawerOpen(GravityCompat.Start))
+                    OnBackPressed();
             };
 
-            
-           
         }
 
         private async void UpdateListView(string query = "getPage?section=popular")
         {
+            progressBar.Visibility = ViewStates.Visible;
+
             var mLayoutManager = new LinearLayoutManager(this);
-            _listView.SetLayoutManager(mLayoutManager);
+            recyclerView.SetLayoutManager(mLayoutManager);
             var adapter = new RecipeAdapter(await UpdateCollectionRecipes(query), this);
             adapter.ItemClick += OnItemClick;
+            recyclerView.SetAdapter(adapter);
 
-            _listView.SetAdapter(adapter);
-            
-            /* FindViewById<RecyclerView>(Resource.Id.listRecipeShorts).Adapter =
-                 new RecipeShortAdapter(this, await UpdateCollectionRecipes(query));*/
+            progressBar.Visibility = ViewStates.Invisible;
         }
 
         public override void OnBackPressed()
@@ -116,7 +118,7 @@ namespace XamarinApp
         private void SetSpinner()
         {
             var spinner = FindViewById<Spinner>(Resource.Id.spinner);
-            spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(SelectedItemSpinner);
 
 
             var spinnerAdapter =
@@ -133,10 +135,14 @@ namespace XamarinApp
             {
                 e.Handled = false;
 
+                page = 1;
+
                 if (e.Event.Action != KeyEventActions.Down || e.KeyCode != Keycode.Enter) return;
 
-                UpdateListView($"getPage?section=recipe&recipeName={edittext.Text}");
+                UpdateListView($"getPage?page={page}&section=recipe&recipeName={edittext.Text}");
                 Toast.MakeText(this, "Загрузка...", ToastLength.Short).Show();
+
+
                 e.Handled = true;
             };
         }
@@ -149,21 +155,23 @@ namespace XamarinApp
         }
 
 
-        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        private void SelectedItemSpinner(object sender, AdapterView.ItemSelectedEventArgs e)
         {
-            var spinner = (Spinner) sender;
+            var spinner = (Spinner)sender;
             var item = spinner.GetItemAtPosition(e.Position);
+
+            page = 1;
 
             switch (item.ToString())
             {
                 case "По популярности":
-                    UpdateListView("getPage?section=popular");
+                    UpdateListView($"getPage?page={page}&section=popular");
                     break;
                 case "По случайности":
-                    UpdateListView("getPage?section=random");
+                    UpdateListView($"getPage?page={page}&section=random");
                     break;
                 case "По новизне":
-                    UpdateListView("getPage?section=new");
+                    UpdateListView($"getPage?page={page}&section=new");
                     break;
             }
 
@@ -174,10 +182,6 @@ namespace XamarinApp
 
         private async Task<RecipeShort[]> UpdateCollectionRecipes(string query) =>
             await Task.Run(function: () => _recipes = HttpGet.GetRecipes(query));
-
-        private RecipeShort[] GetRecipes(string query) => UpdateCollectionRecipes(query).Result;
-
-
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions,
             [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
