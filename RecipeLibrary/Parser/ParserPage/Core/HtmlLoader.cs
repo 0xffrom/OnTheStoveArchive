2d6699855675a963ba88f051180e1be3b661d149
Андрей Романюк <asromanyuk@edu.hse.ru>
2020-04-25 14:39:01 +0300

    Поправлена ошибка с получением страниц рецептов.

--- RecipeLibrary/Parser/ParserPage/Core/HtmlLoader.cs
+++ RecipeLibrary/Parser/ParserPage/Core/HtmlLoader.cs
@@ -21,28 +21,28 @@ namespace ObjectsLibrary.Parser.ParserPage.Core
 
         public async Task<string> GetSource(int idPage, string recipeName)
         {
-            var currentUrl = _url;
+            string currentUrl = _url;
 
             switch (_settings.Section)
             {
                 case ("new"):
-                    currentUrl = _settings.SuffixNew;
+                    currentUrl += _settings.SuffixNew;
                     break;
                 case ("popular"):
-                    currentUrl = _settings.SuffixPopular;
+                    currentUrl += _settings.SuffixPopular;
                     break;
                 case ("recipe"):
-                    currentUrl = _settings.SuffixRecipe;
+                    currentUrl += _settings.SuffixRecipe;
                     break;
                 default:
                     // Если есть поиск по тегу:
                     if (_settings.Sections.ContainsKey(_settings.Section))
                     {
-                        currentUrl = _settings.Sections[_settings.Section];
+                        currentUrl += _settings.Sections[_settings.Section];
                         break;
                     }
                     
-                    currentUrl = _settings.SuffixNew;
+                    currentUrl += _settings.SuffixNew;
                     break;
             }
 
