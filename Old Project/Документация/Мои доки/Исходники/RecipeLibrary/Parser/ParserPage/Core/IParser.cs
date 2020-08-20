f9f604ad1ef2257b9d3395711a8c93e577c766e5
andrew <asromanyuk@edu.hse.ru>
2020-08-20 15:41:29 +0300

    change folder

--- Old Project/Документация/Мои доки/Исходники/RecipeLibrary/Parser/ParserPage/Core/IParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using AngleSharp.Html.Dom;
-
-namespace ObjectsLibrary.Parser.ParserPage.Core
-{
-    internal interface IParserPage<out T> where T : class
-    {
-        T Parse(IHtmlDocument document, IParserPageSettings settings);
-    }
-}
